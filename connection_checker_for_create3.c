7fcee7b2318494846b255a89c71682033a67387e
arvindiscoder <161108992+arvindiscoder@users.noreply.github.com>
2024-03-29 20:30:34 -0700

    added comments

--- connection_checker_for_create3.c
+++ connection_checker_for_create3.c
@@ -1,8 +1,20 @@
+//    Title:                    Line Follow
+//    Files:                   kipr/wombat.h, stdio.h
+//    Semester:           Rancho Botball Spring 2024
+//
+//    Author:                Arvind Sainathan
+//    Pair Partner(s)   Krishnaa Rajesh
+//    Email:                  as91657@student.musd.org
+//    Coach:                Mr. Tran
+//    Credits:               Mr. Tim
+//
+//
 #include <kipr/wombat.h>
+
 
 int main()
 {
-    printf("Hello World\n");
-    create3_connect();
+    printf("Hello World\n"); // Checks to see if wombat works
+    create3_connect(); // will try to connect to the Create 3 and display if the Create 3 is connected
     return 0;
 }
